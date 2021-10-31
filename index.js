ca3ec201f97f5efbc5dbd000fba5e1d75b4e6d99
Amirul-Hosain <amirulhosain77@gmail.com>
2021-10-31 21:21:49 +0600

    manage and oreder added

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ async function run() {
       res.json(result)
     })
 
-    // DELETE a single manageOrder
+    // DELETE a single Order
     app.delete('/orders/:id', async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
